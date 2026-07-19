53d0b7bf0f98693a9532b6f74cae4353627e730e
favourkendi-dev <favour.kendi@student.moringaschool.com>
2026-07-19 21:03:50 +0300

    set up vite react and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-violet-600">DevLink</h1>
+    </div>
+  )
+}
+
+export default App
